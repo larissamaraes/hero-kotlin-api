57404b70c27ca0a1c7baffb1a1a0b03aa2b4df40
Larissa Marães <larissa.maraes@gmail.com>
2018-11-11 01:44:42 -0900

    fixing id annotation for hero entity

--- src/main/kotlin/com/larissa/maraes/herokotlinapi/entity/Hero.kt
+++ src/main/kotlin/com/larissa/maraes/herokotlinapi/entity/Hero.kt
@@ -1,11 +1,11 @@
 package com.larissa.maraes.herokotlinapi.entity
 
 import com.fasterxml.jackson.annotation.JsonFormat
-import org.springframework.data.annotation.Id
 import java.util.*
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
+import javax.persistence.Id
 
 @Entity
 data class Hero(
